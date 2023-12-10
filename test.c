e628e329a470701558083782bcf62c1379ef2827
Matisse <legendre.matisse@gmail.com>
2023-12-10 17:04:53 +0100

    banaan

--- test.c
+++ test.c
@@ -27,13 +27,13 @@ int	main(void)
 	t_data	img;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "First MLX test");
-	img.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_win = mlx_new_window(mlx, 920, 800, "First MLX test");
+	img.img = mlx_new_image(mlx, 920, 800);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
-	for (int j = 0; j < 1080; j+=25)
+	for (int j = 0; j < 800; j+=25)
 	{
-		for (int i = 0; i < 1920; i++)
+		for (int i = 0; i < 920; i++)
 			my_mlx_pixel_put(&img, i, j, 0x00FF0000);
 	}
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
