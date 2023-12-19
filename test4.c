e3a4a02868ade72e91e30ef32daccc30ba55b382
LegendreMatisse <legendre.matisse@gmail.com>
2023-12-20 00:45:35 +0100

    Update test4.c

--- test4.c
+++ test4.c
@@ -61,8 +61,7 @@ int main(void)
     mlx.mlx = mlx_init();
     mlx.win = mlx_new_window(mlx.mlx, 500, 500, "Create image test 2");
     mlx.img = mlx_new_image(mlx.mlx, 500, 500);
-    mlx.addr = mlx_get_data_addr(mlx.img, &mlx.bits_per_pixel, &mlx.line_length,
-                                &mlx.endian);
+    mlx.addr = mlx_get_data_addr(mlx.img, &mlx.bits_per_pixel, &mlx.line_length, &mlx.endian);
     draw_fractal(&mlx);
     mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
     mlx_hook(mlx.win, 2, 1L<<0, keypress, &mlx);
