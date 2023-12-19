8e67236dd7259d70e0499533c2776add49ef6183
LegendreMatisse <legendre.matisse@gmail.com>
2023-12-19 23:20:16 +0100

    Update test4.c

--- test4.c
+++ test4.c
@@ -5,7 +5,7 @@
 void    cool_fractol_function(t_data *img, t_mlx *mlx)
 {
     int x = 0, y = 0;
-    int color = 0xffff00ff;
+    int color = 0xED0800;
     if (img->bits_per_pixel != 32)
         color = mlx_get_color_value(mlx->mlx, color);
     while (y < 500)
