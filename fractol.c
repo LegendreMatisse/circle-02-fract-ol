968413620eb73769fbabedeb9854d54f3311a1b4
LegendreMatisse <legendre.matisse@gmail.com>
2024-01-05 00:12:35 +0100

    Norm fix fractol.c

--- fractol.c
+++ fractol.c
@@ -12,31 +12,31 @@
 
 #include "fractol.h"
 
-void    init_fractal(t_mlx *mlx)
+void	init_fractal(t_mlx *mlx)
 {
-    mlx->x = 0;
-    mlx->y = 0;
-    mlx->color = 0x0bd2024;
-    mlx->zoom = 300;
-    mlx->offset_x = -1.21;
-    mlx->offset_y = -1.21;
-    mlx->max_iterations = 100;
-    mlx->bits_per_pixel = 32;
-    mlx->line_length = SIZE * 4;
-    mlx->endian = 0;
+	mlx->x = 0;
+	mlx->y = 0;
+	mlx->color = 0x0bd2024;
+	mlx->zoom = 300;
+	mlx->offset_x = -1.21;
+	mlx->offset_y = -1.21;
+	mlx->max_iterations = 100;
+	mlx->bits_per_pixel = 32;
+	mlx->line_length = SIZE * 4;
+	mlx->endian = 0;
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_mlx   *mlx;
+	t_mlx	*mlx;
 
-    (void)argc;
-    mlx = malloc(sizeof(t_mlx));
-    init_fractal(mlx);
-    init_mlx(mlx);
-    mlx_hook(mlx->win, 2, 1L<<0, keypress, mlx);
+	(void)argc;
+	mlx = malloc(sizeof(t_mlx));
+	init_fractal(mlx);
+	init_mlx(mlx);
+	mlx_hook(mlx->win, 2, 1L<<0, keypress, mlx);
 	mlx_hook(mlx->win, 17, 1L<<17, ft_exit_wo_mess, mlx);
-    draw_fractal(mlx, argv[1], ft_atof(argv[2]), ft_atof(argv[3]));
+	draw_fractal(mlx, argv[1], ft_atof(argv[2]), ft_atof(argv[3]));
 	mlx_loop(mlx->mlx);
-    return (0);
+	return (0);
 }
