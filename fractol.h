3d2aa070c6d6e3d680240af5b7117c20dba09294
LegendreMatisse <legendre.matisse@gmail.com>
2024-01-07 16:30:05 +0100

    Clean up and small changes
    Removal of t_data struct, as it was replaced by t_mlx struct. Renamed some of the variables in the t_mlx struct to help with norm limits.

--- fractol.h
+++ fractol.h
@@ -26,23 +26,14 @@
 
 # define SIZE 700
 
-typedef struct s_data
-{
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
 typedef struct s_mlx
 {
 	void	*mlx;
 	void	*win;
 	void	*img;
 	void	*addr;
-	int		bits_per_pixel;
-	int		line_length;
+	int		bpp;
+	int		line_len;
 	int		endian;
 	int		x;
 	int		y;
