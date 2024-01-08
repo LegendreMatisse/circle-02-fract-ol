4ea741d92c9d5257fbf327bab0faec5d957c6a5d
LegendreMatisse <legendre.matisse@gmail.com>
2024-01-08 12:42:47 +0100

    Update julia.c

--- julia.c
+++ julia.c
@@ -22,7 +22,8 @@ void	init_julia(t_mlx *mlx, double x, double y)
 	//mlx->zoom = 275;
 	//mlx->offset_x = -1.21;
 	mlx->offset_x = -1.21 - 250 / mlx->zoom;
-	mlx->offset_y = -1.21;
+	//mlx->offset_y = -1.21;
+	mlx->offset_y = -1.21 - 250 / mlx->zoom;
 	mlx->max_iterations = 100;
 	mlx->bpp = 32;
 	mlx->line_len = SIZE * 4;
