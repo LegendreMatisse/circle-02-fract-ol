a07b8338f1bbf8894c32d73d27816dd0dfdf0467
LegendreMatisse <legendre.matisse@gmail.com>
2024-01-08 23:52:54 +0100

    fix part 3

--- utils_3.c
+++ utils_3.c
@@ -20,4 +20,12 @@ void	change_iterations(t_mlx *mlx, char direction)
 		mlx->max_iterations -= 1;
 }
 
-int
+int	redraw(t_mlx *mlx)
+{
+	mlx_destroy_image(mlx->mlx, mlx->img);
+	mlx->img = mlx_new_image(mlx->mlx, SIZE, SIZE);
+	mlx->addr = mlx_get_data_addr(mlx->img, &mlx->bpp, &mlx->line_len,
+			&mlx->endian);
+	draw_fractal(mlx, mlx->name, 'k');
+	return (0);
+}
