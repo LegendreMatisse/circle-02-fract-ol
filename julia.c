3f19c7803cb06725f3e8c84492746c16880b743b
LegendreMatisse <legendre.matisse@gmail.com>
2024-01-08 13:19:24 +0100

    Update julia.c

--- julia.c
+++ julia.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "fractol.h"
+#include <stdio.h>
 
 void	init_julia(t_mlx *mlx, double x, double y, char function)
 {
@@ -18,7 +19,9 @@ void	init_julia(t_mlx *mlx, double x, double y, char function)
 	{
 		mlx->zoom = 200;
 		mlx->offset_x = -1.21;
+		printf("offset_x = %f\n", mlx->offset_x);
 		mlx->offset_y = -1.21;
+		printf("offset_y = %f\n", mlx->offset_y);
 	}
 	mlx->x = 0;
 	mlx->y = 0;
